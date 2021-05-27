4f1aa3107898ef1710abd53d95c03c0d7a2cbf49
folex <0xdxdy@gmail.com>
2021-05-27 16:14:41 +0300

    fix workspace dir

--- download_examples.sh
+++ download_examples.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-mkdir -p fluence || true
-git clone --depth 1 https://github.com/fluencelabs/examples.git fluence/examples || true
-git clone --depth 1 https://github.com/fluencelabs/aqua-playground.git fluence/aqua-playground || true
-git clone --depth 1 https://github.com/fluencelabs/fluentpad.git fluence/fluentpad || true
+git clone --depth 1 https://github.com/fluencelabs/examples.git examples || true
+git clone --depth 1 https://github.com/fluencelabs/aqua-playground.git aqua-playground || true
+git clone --depth 1 https://github.com/fluencelabs/fluentpad.git fluentpad || true
 
 echo '
 =======
